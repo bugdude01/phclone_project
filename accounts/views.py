ce3803cdb3c10d81cd8d51a9d5e9a185f3a86331
bugdude01 <fafrhodes@yahoo.co.uk>
2018-03-27 14:26:29 +0100

    add login, signup and logout

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def signup(request):
+    return render(request, 'accounts/signup.html')
+
+def login(request):
+    return render(request, 'accounts/login.html')
+
+def logout(request):
+    #need to add route to homepabe and don't forget to logout
+    return render(request, 'accounts/signup.html')
