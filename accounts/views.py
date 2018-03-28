ef3e51aaa672d2e397197042ae239f61390a58cb
bugdude01 <fafrhodes@yahoo.co.uk>
2018-03-28 12:16:25 +0100

    products model added

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 
 
 def signup(request):
-    return render(request, 'accounts/signup.html')
+    if request.method == 'POST':
+        #The user wnts to sign up!
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.get(username=request.POST['username'])
+                return render(request, 'accounts/signup.html', {'error':'Sorry! That username has already been taken. Please use a different one.'})
+            except User.DoesNotExist:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                auth.login(request, user)
+                return redirect('home')
+        else:
+            return render(request, 'accounts/signup.html', {'error':'The passwords need to match. Please try again.'})
+            
+    else:
+        
+        return render(request, 'accounts/signup.html')
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html',{'error':'username or pasword is incorrect.'})
+    else:
+        return render(request, 'accounts/login.html')
 
 def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
     #need to add route to homepabe and don't forget to logout
     return render(request, 'accounts/signup.html')
